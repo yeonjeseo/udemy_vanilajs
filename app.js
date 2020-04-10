3b84b0ea5d86892b1b8bbcac06e33b27a9070e36
yeonjeseo <yeonjeseo@hotmail.com>
2020-04-10 14:34:34 +0900

    Type Conversion

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-//PRIMITIVE TYPES
+let val;
 
-//String
-const name = 'Yeon Jeffrey Seo';
-//Number
-const age = 31;
-//Boolean
-const getMarried = false;
-//Null
-const car = null;
-//Undefined
-let idiot;
-//Symbol
-const sym = Symbol();
+//Number to String
+val = String(5);
+val = String(4+4);
+//Bool to String
+val = String(true);
+//Date to String
+val = String(new Date());
+//Array to String
+val = [1,2,3,4,5,6];
+//toString();
+val = (10).toString();
+val = (new Date()).toString();
 
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof getMarried);
-console.log(typeof car);
-console.log(typeof idiot);
-console.log(typeof sym);
-console.clear();
 
-//REFERENCE TYPES - Objects
-//Array
-const hobbies = ['Watching YouTube', 'Basketball'];
-//Object Literal
-const address = {
-    city : 'Seoul',
-    road : 'Nangok-ro'
-}
-const today = new Date();
+//String to number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number('Hello');
+val = Number([1,2,3,4,5]);
+val = Number(null);
 
-console.log(today);
-console.log(typeof hobbies);
-console.log(typeof address);
-console.log(typeof today);
+val = parseInt('100.24');
+val = parseFloat('3.1415');
+
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+// console.log(val.toFixed(2));
+
+const val1 = String(5);
+const val2 = 6;
+
+const sum =val1 + val2;
+console.log(sum);
+console.log(typeof sum);
