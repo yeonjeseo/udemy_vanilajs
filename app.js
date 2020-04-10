0647cdc7090c604225923205c62e477fac169c6a
yeonjeseo <yeonjeseo@hotmail.com>
2020-04-10 15:29:24 +0900

    String Methods & Concatenation

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
-const val1 = 100;
-const val2 = 60;
+const firstName = 'Yeon';
+const middleName = 'Jeffrey';
+const lastName = 'Seo';
+const str = 'Hello there, my name is Yeon Jeffrey Seo!';
+const tags = 'web development, web design, programming, frontend, backend';
 
 let val;
-
-//Simple Math with Numbers
-val = val1 + val2;
-val = val1 - val2;
-val = val1 * val2;
-val = val1 / val2;
-val = val1 % val2;
-
-//MATH Object
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.4);
-val = Math.round(2.5);
-val = Math.floor(2.9);
-val = Math.ceil(2.2);
-val = Math.sqrt(144);
-val = Math.abs(-34);
-val = Math.pow(2,5);
-val = Math.min(-2, 3, 7, 33, 82);
-val = Math.max(-2, 3, 7, 33, 82);
-val = Math.random();
-val = Math.random() * 20;
-val = Math.floor(Math.random() * 20 + 1);
-
+//Concatenation
+val = firstName + ' ' + middleName + ' ' + lastName;
+//Append : Not replace it, but Add on to it.
+val = 'Yeon Jeffrey ';
+val += 'Seo';
+//Escaping
+val = "That's Awesome. I can't wait!";
+val = 'That\'s Awesome. I can\'t wait!';
+//Length
+val=firstName.length;
+//concat
+val = firstName.concat(' ', middleName,' ',lastName);
+//Change Case
+val = firstName.toUpperCase();
+val = lastName.toLowerCase();
+val = firstName[0];
+//indexOf()
+val = middleName.indexOf('f');
+val = middleName.lastIndexOf('f');
+//charAt()
+val = middleName.charAt('0');
+//Get last character (using charAT & length)
+val = middleName.charAt(middleName.length - 1);
+//substring()
+val = middleName.substring(0,5);
+//slice()
+val = middleName.slice(0,4);
+val = middleName.slice(-3);
+//split() -- returns an array!!!
+val = str.split(' ');
+val = tags.split(',');
+//replace()
+val = str.replace('Seo', 'Ryu');
+//include()
+val = str.includes('Hello');
+val = str.includes('Hi');
 console.log(val);
