df5e78d03e6d9ade2313aa4d66dc40870aa23aee
yeonjeseo <yeonjeseo@hotmail.com>
2020-04-13 19:05:42 +0900

    Block Scope With let & const

--- app.js
+++ app.js
@@ -1,58 +1,24 @@
-// WINDOW METHODS / OBJECTS / PROPERTIES
+// Global Scope
+var a = 1;
+let b = 2;
+const c = 3;
+// function test() {
+//     var a = 4;
+//     let b = 5;
+//     const c = 6;
+//     console.log("Function Scope : ", a, b, c);
+// }
+// test();
 
-window.console.log(1234);
-
-// Alert
-// alert('Hello World!');
-
-// Prompt
-// const input = prompt();
-// alert(input);
-
-// Confirm
-// if(confirm('Are you sure?')) {
-//     console.log('YES');
-// } else {
-//     console.log('NO');
+// if (true) {
+//     //Block Scope
+//     var a = 4;
+//     let b = 5;
+//     const c = 6;
+//     console.log("If Scope : ", a, b, c);
+// }
+// for (let a = 0; a < 10; a++) {
+//     console.log(`Loop :  ${a}`);
 // }
 
-let val;
- 
-// Outer height abd width
- val = window.outerHeight;
- val = window.outerWidth;
-
- // Inner height and width;
- val = window.innerHeight;
- val = window.innerWidth;
-
- // Scroll Points
- val = window.scrollY;
- val = window.scrollX;
-
- // Locatgion Object
- val = window.location;
- val = window.location.hostname;
- val = window.location.port;
- val = window.location.href;
- val = window.location.search;
-
-// Redirect
-// window.location.href = 'https://google.com'; 
-// Reload
-// window.location.reload();
-
-// History Object
-// window.history.go(-3);
-// val = window.history.length;
-
-// Navigator Object
-val = window.navigator;
-val = window.navigator.appName;
-val = window.navigator.appVersion;
-val = window.navigator.userAgent;
-val = window.navigator.platform;
-val = window.navigator.vendor;
-val = window.navigator.language;
-
-console.log(val);
+console.log("Globla Scope : ", a, b, c);
