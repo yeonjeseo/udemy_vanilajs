18b06e13fd48076c603b0300738ee4a9629e4043
yeonjeseo <yeonjeseo@hotmail.com>
2020-04-13 21:23:47 +0900

    DOM Selectors For Multiple Elements

--- app.js
+++ app.js
@@ -1,49 +1,46 @@
-// document.getElementById()
+// document.getElementsByClassName
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[3].textContent = 'Hello';
 
-// console.log(document.getElementById('task-title'));
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// console.log(listItems);
 
-// // Get things from the element
-// console.log(document.getElementById('task-title').id);
-// console.log(document.getElementById('task-title').className);
+// document.getElementsByTagName
+// let lis = document.getElementsByTagName('li');
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = 'red';
+// lis[3].textContent = 'Hello';
 
+// // Convert HTML Collection into array
+// lis = Array.from(lis);
+// lis.reverse();
+// lis.forEach(function(li, index){
+//     console.log(li.className);
+//     li.textContent = `${index} : Hello!`;
+// })
+// console.log(lis);
 
-// // Change styling
-// document.getElementById('task-title').style.background = 'black';
-// document.getElementById('task-title').style.color = '#fff';
-// document.getElementById('task-title').style.padding = '5px';
-// // document.getElementById('task-title').style.display = 'none';
+// document.querySelectorAll
+const items = document.querySelectorAll("ul.collection li.collection-item");
+console.log(items);
 
-// // Change content
-// document.getElementById('task-title').textContent = 'Task List';
-// document.getElementById('task-title').innerText = 'My Tasks';
-// document.getElementById('task-title').innerHTML = '<span style="color:red">Task Lisk</span';
+items.forEach(function (items, index) {
+    items.textContent = `${index} : Hello!!!!`;
+});
 
-// Cast by variable
-// const taskTitle = document.getElementById('task-title');
-// taskTitle.style.background = 'black';
-// taskTitle.style.color = '#fff';
-// taskTitle.style.padding = '5px';
-// taskTitle.textContent = 'Task List';
-// taskTitle.innerText = 'My Tasks';
-// taskTitle.innerHTML = '<span style="color:red">Task Lisk</span';
+const liOdd = document.querySelectorAll("li:nth-child(odd)");
+const liEven = document.querySelectorAll("li:nth-child(even)");
 
-// document.querySelector()
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5'));
+liOdd.forEach(function (li, index) {
+    li.style.background = " #ccc";
+});
 
-// Single Element Selector!!! Remember this!!
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
-document.querySelector('li:last-child').style.color = 'red';
-document.querySelector('li:nth-child(3)').style.color = 'yellow';
-document.querySelector('li:nth-child(4)').textContent = 'Hello World';
-document.querySelector('li:nth-child(odd)').style.background = '#ccc';
-document.querySelector('li:nth-child(even)').style.background = '#ccc';
+for (let i = 0; i < liEven.length; i++) {
+    liEven[i].style.background = "#f4f4f4";
+}
 
-
-
-
-
-
-
+console.log(items);
