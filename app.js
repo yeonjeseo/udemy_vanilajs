bd79b8be46930ddedabad83787a3eee80f6294d8
yeonjeseo <yeonjeseo@hotmail.com>
2020-04-14 15:52:57 +0900

    Event Listeners & The Event Object

--- app.js
+++ app.js
@@ -1,52 +1,40 @@
-// REPLACE ELEMENT
+// document.querySelector('.clear-tasks').addEventListener('click',
+// function(e){
+//     console.log('Hello World!!');
+//     //e.preventDefault();
+// });
+// href="" -- link to the same page
+// href="#" -- hash symbol, going to stop it from actually making the browser redirect
 
-// Create Element
-const newHeading = document.createElement('h2');
-// Add id 
-newHeading.id = 'task-title';
-// New text node
-newHeading.appendChild(document.createTextNode('Task Lisk'));
+document.querySelector(".clear-tasks").addEventListener("mouseover", onclick);
 
-// Get the old heading
-const oldHeading = document.getElementById('task-title');
-// Parent
-const cardAction = document.querySelector('.card-action');
+function onclick(e) {
+    // console.log('Clicked');
+    let val;
+    val = e;
 
-// Replace
-cardAction.replaceChild(newHeading, oldHeading);
+    // Event target element
+    val = e.target;
+    val = e.target.id;
+    val = e.target.className;
+    val = e.target.classList;
 
-// ReEMOVE ELEMENT
-const lis = document.querySelectorAll('li');
-const list = document.querySelector('ul');
+    e.target.innerText = 'Hello';
 
-// Remove list item
-lis[0].remove();
+    // Event Type
+    val = e.type;
 
-// Remove child element
-list.removeChild(lis[3]);
+    // Timestamp
+    val =e.timeStamp;
 
-// CLASSES & ATTRIBUTE
-const firstLi = document.querySelector('li:first-child');
-const link = firstLi.children[0];
+    // Coords event relative to the window
+    val = e.clientY;
+    val = e.clientX;
 
-let val;
-console.log(link);
-// Classes
-val = link.className;
-val = link.classList;
-val = link.classList[0];
-link.classList.add('test');
-link.classList.remove('test');
-val = link;
+    // Coords event relative to the element
+    val = e.offsetY;
+    val = e.offsetX;
 
-// Attribute
-val = link.getAttribute('href');
-console.log(val);
-val = link.setAttribute('href', 'https://google.com');
-val = link.hasAttribute('href');
-link.setAttribute('title', 'Google');
-val = link.hasAttribute('title');
-link.removeAttribute('title');
-val = link.hasAttribute('title');
 
-console.log(val);
+    console.log(val);
+}
